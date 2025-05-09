4d2342dcfdf096e187cabb8d1c463fb92d6e96af
sportline105 <norbert616@freenet.de>
2025-05-09 23:31:20 +0200

    -

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.9.99',
+            'typo3' => '8.5.0-12.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
